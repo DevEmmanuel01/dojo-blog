89875549883b676d3ece144761fd9723f884df4a
DevEmmanuel01 <gboadekola2003@gmail.com>
2022-04-03 15:03:19 +0100

    Used exact match and router link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
 // Full react tutorial #5 - Multiple COmponets
 function App() {
   return (
-    <div className="App">
-      {/* Nesting the navbar in the template */}
-      {/*in js*/}
-      <Navbar />
-      {/* Nesting the HomePage in the template */}
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        {/* Nesting the navbar in the template */}
+        {/*in js*/}
+        <Navbar />
+        {/* Nesting the HomePage in the template */}
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
