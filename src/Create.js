ee896d11c1dfd4b733491aed8eb858e9bae9d38d
DevEmmanuel01 <gboadekola2003@gmail.com>
2022-04-11 08:01:22 +0100

    Reusing custom hooks and controlled inputs

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,19 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input type="text" required />
+        <label>Blog Body:</label>
+        <textarea>required</textarea>
+        <label>Blog Author:</label>
+        <select>
+          <option value="select">Select</option>
+          <option value="Gbolahan">Gbolahan</option>
+          <option value="Emmanuel">Emmanuel</option>
+        </select>
+        <button>Add Me</button>
+      </form>
     </div>
   );
 };
